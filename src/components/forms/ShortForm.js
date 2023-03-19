c19f7257beb195958dc6b4d99da3daf0d35ab758
vivi108 <sunmyoung61@gmail.com>
2023-03-19 20:51:12 +0900

    add : ShortForm

--- src/components/forms/ShortForm.js
+++ src/components/forms/ShortForm.js
@@ -1,14 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
 //주관식
-function ShortForm() {
-    const [answer, setAnswer] = useState("");
-    return (
-    <div>
-        
+function ShortForm(props) {
 
+
+  const textRef = React.useRef();
+  const [answer, setAnswer] = useState("");
+  const onChange = (e) => {
+    setAnswer(e.target.value);
+  };
+  React.useEffect(() => {
+    if (textRef && textRef.current) {
+      textRef.current.style.height = "100px";
+      const taHeight = textRef.current.scrollHeight;
+      textRef.current.style.height = taHeight + "px";
+    }
+  }, [answer]);
+
+  return (
+    <div>
+      
+        <Form.Group>
+          <Form.Control as="textarea" onChange={onChange} ref={textRef} />
+        </Form.Group>
     </div>
-  )
+  );
 }
 
-export default ShortForm
+export default ShortForm;
