91be933202a0898f27407422c3f1153c85662b76
chromato99 <chromato99@gmail.com>
2023-03-19 20:49:58 +0900

    Add delete selection button

--- src/components/forms/QuestionForm.js
+++ src/components/forms/QuestionForm.js
@@ -1,14 +1,12 @@
-import React , {useState} from "react";
+import React from "react";
 import Card from "react-bootstrap/Card"
 import Button from "react-bootstrap/Button"
-import {useNavigate} from "react-router-dom";
 
 import RadioButton from './RadioButton'
 import ShortForm from './ShortForm'
 import Checkbox from './Checkbox'
 
 function QuestionForm(props) {
-  const navigate = useNavigate();
   const questionType = props.questiontype;
 
   return (
@@ -17,9 +15,9 @@ function QuestionForm(props) {
         <Card.Title>{props.q.questionTitle}</Card.Title>
         <Button variant="outline-danger" onClick={(e)=>{props.delfunction(props.q.id)}}>X</Button>
           <div className="questionType">
-            {questionType==1 && <RadioButton q={props.q} questions={props.questions} setQuestions={props.setQuestions}/>}
-            {questionType==2 && <Checkbox/>}
-            {questionType==3 && <ShortForm/>}
+            {questionType===1 && <RadioButton q={props.q} questions={props.questions} setQuestions={props.setQuestions}/>}
+            {questionType===2 && <Checkbox/>}
+            {questionType===3 && <ShortForm/>}
           </div>
       </Card.Body>
     </Card>
--- src/components/forms/RadioButton.js
+++ src/components/forms/RadioButton.js
@@ -4,28 +4,35 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function RadioButton(props) {
+  function addSelection() {
+    props.questions[props.q.id].item.push(null);
+    props.setQuestions([ ...props.questions ]);
+  }
+
+  function delSelection() {
+    props.questions[props.q.id].item.pop();
+    props.setQuestions([ ...props.questions ]);
+  }
+
   return (
     <Form.Group>
-      {props.q.item.map((selection, index) => (
-        <div key={`radio-${props.q.questionTitle}-${index}`} className="mb-3" style={{ display: "flex" }}>
+      {props.q.item.map((item, index) => (
+        <div key={`question-${props.q.id}-${index}`} className="mb-3" style={{ display: "flex" }}>
           <InputGroup>
             <InputGroup.Checkbox
               disabled
               type={"radio"}
-              name={`radio-${props.q.questionTitle}`}
-              id={`radio-${props.q.questionTitle}-${selection}`}
+              name={`radio-${props.q.id}`}
+              id={`radio-${props.q.id}-${index}`}
             />
-            <Form.Control type="text" defaultValue={selection} placeholder="Enter selection" onChange={(e) => {
+            <Form.Control type="text" defaultValue={item} placeholder="Enter selection" onChange={(e) => {
               props.q.item[index] = e.target.value;
             }} />
           </InputGroup>
         </div>
       ))}
-      <Button onClick={() => {
-        console.log(props.q.item);
-        props.questions[props.q.id].item.push(null);
-        props.setQuestions([...props.questions]);
-      }}>Add Selection</Button>
+      <Button onClick={addSelection}>Add Selection</Button>
+      <Button onClick={delSelection}>Delete Selection</Button>
     </Form.Group>
   )
 }
