ead6b0d3ccea4e3a2ba73152ed83af4aa5725f19
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-04-10 19:59:07 +0900

    fix message and button margin (#10)

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,27 +1,25 @@
 import React, { useContext, useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import "./Login.css";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { AuthContext } from "../App";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-
   const navigate = useNavigate();
 
-  const [userId, setUserId] = useState('');
-  const [userPassword, setUserPassword] = useState('');
+  const [userId, setUserId] = useState("");
+  const [userPassword, setUserPassword] = useState("");
   const { setIsLoggedIn } = useContext(AuthContext);
 
   const idChange = (e) => {
     setUserId(e.target.value);
-  }
+  };
   const passwordChange = (e) => {
     setUserPassword(e.target.value);
-  }
+  };
 
-  useEffect(() => {
-  }, []);
+  useEffect(() => {}, []);
 
   const loginClick = () => {
     axios
@@ -29,67 +27,52 @@ export default function Login() {
         "/user/login",
         {
           userId,
-          userPassword
+          userPassword,
         },
-        { 'Content-Type': 'application/json' }
+        { "Content-Type": "application/json" }
       )
       .then((res) => {
-        alert("Success!");
+        alert("로그인 되었습니다!");
         setIsLoggedIn(true);
         localStorage.setItem("isLoggedIn", true);
         navigate("/");
       })
       .catch((err) => {
-        if (err.response.status === 403){
-          alert("Invalid User")
-        }
-        else{
+        if (err.response.status === 403) {
+          alert("Invalid User");
+        } else {
           alert(err);
         }
       });
-
-  }
+  };
   const enterKeyPress = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       loginClick();
     }
-  }
+  };
   const register = (e) => {
-    navigate("/register")
-  }
+    navigate("/register");
+  };
 
   return (
     <div className="Auth-form-container">
-      <div className="Auth-form" >
+      <div className="Auth-form">
         <div className="Auth-form-content">
-          <div className="sharpic">
-          </div>
+          <div className="sharpic"></div>
           <br></br>
           <div className="form-group mt-3">
             <label>ID</label>
-            <input
-              type="text"
-              name="id"
-              className="form-control mt-1"
-              placeholder="Enter id"
-              onChange={idChange}
-            />
+            <input type="text" name="id" className="form-control mt-1" placeholder="Enter id" onChange={idChange} />
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
-            <input
-              type="password"
-              className="form-control mt-1"
-              placeholder="Enter password"
-              onChange={passwordChange}
-              onKeyDown={enterKeyPress}
-            />
+            <input type="password" className="form-control mt-1" placeholder="Enter password" onChange={passwordChange} onKeyDown={enterKeyPress} />
           </div>
           <br></br>
           <br></br>
           <div className="text-center">
             Not registered yet?{" "}
-            <span className="link-primary" onClick={register} style={{cursor: "pointer"}}>
+            <span className="link-primary" onClick={register} style={{ cursor: "pointer" }}>
               Sign Up
             </span>
           </div>
@@ -101,7 +84,5 @@ export default function Login() {
         </div>
       </div>
     </div>
-
-
   );
 }
--- src/components/Survey.js
+++ src/components/Survey.js
@@ -6,15 +6,13 @@ import AddingOption from "./forms/AddingOption";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import { FormHandlingContext } from "../App";
-import Axios from 'axios';
+import Axios from "axios";
 
 export default function Survey() {
-
   const [data, setData] = useState({});
   const [surveyAnswer, setSurveyAnswer] = useState();
 
   useEffect(() => {
-    
     // Axios.get(`http://localhost:8080/survey/${data.surveyPk}/`).then((response) => {
     //   setData(response.data);
     // });
@@ -24,32 +22,28 @@ export default function Survey() {
       surveyPk: 1,
       surveyTitle: "test",
       surveyDescription: "test survey",
-      questions: "[{\"id\": 0,\"questionTitle\": \"test question\",\"questionType\": 1, \"item\": [\"test1\", \"test2\", \"test3\"]},{\"id\": 1,\"questionTitle\": \"test question2\",\"questionType\": 2,\"item\": [\"test4\", \"test5\", \"test6\"]},{\"id\": 3,\"questionTitle\": \"test question3\",\"questionType\": 3,\"item\": [\"test4\"]}]",
-      author: 1
+      questions:
+        '[{"id": 0,"questionTitle": "test question","questionType": 1, "item": ["test1", "test2", "test3"]},{"id": 1,"questionTitle": "test question2","questionType": 2,"item": ["test4", "test5", "test6"]},{"id": 3,"questionTitle": "test question3","questionType": 3,"item": ["test4"]}]',
+      author: 1,
     };
 
     response = {
       ...response,
-      questions: JSON.parse(response.questions)
-    }
-    setData(response)
+      questions: JSON.parse(response.questions),
+    };
+    setData(response);
 
     let answerForm = {
       surveyPk: response.surveyPk,
       userPK: 1,
-      answer: Array.from(
-        { length: response.questions.length },
-        (_, i) => Array.from(
-          { length: response.questions[i].item.length },
-          () => false
-        )
-      )
-    }
+      answer: Array.from({ length: response.questions.length }, (_, i) => Array.from({ length: response.questions[i].item.length }, () => false)),
+    };
     console.log(answerForm);
     setSurveyAnswer(answerForm);
   }, []);
 
-  function handleSubmit(e) { // 버튼누르면 응답 제출 
+  function handleSubmit(e) {
+    // 버튼누르면 응답 제출
     e.preventDefault();
     console.log(surveyAnswer);
 
@@ -62,37 +56,21 @@ export default function Survey() {
   return (
     <Container className="CreateSurvey">
       <div className="text-wrapper">
-        <div className="surveyTitle">
-          {data.surveyTitle}
-        </div>
-        <div className="surveyDesc">
-          {data.surveyDescription}
-        </div>
+        <div className="surveyTitle">{data.surveyTitle}</div>
+        <div className="surveyDesc">{data.surveyDescription}</div>
       </div>
       <Form className="Form" onSubmit={handleSubmit}>
         <div className="ButtonWrapper">
           <div className="SurveyBtnWrapper">
-            <Button
-              className="submit-btn"
-              type="submit"
-              variant="outline-success"
-            >
+            <Button className="submit-btn" type="submit" variant="outline-success">
               Submit Answer
             </Button>
           </div>
         </div>
-        {data.questions && data.questions.map((q, index) => {
-          return (
-            <QuestionForm
-              forCreate={false}
-              questionType={q.questionType}
-              q={q}
-              qIndex={index}
-              key={q.id}
-              answer={surveyAnswer.answer[index]}
-            />
-          );
-        })}
+        {data.questions &&
+          data.questions.map((q, index) => {
+            return <QuestionForm forCreate={false} questionType={q.questionType} q={q} qIndex={index} key={q.id} answer={surveyAnswer.answer[index]} />;
+          })}
       </Form>
     </Container>
   );
