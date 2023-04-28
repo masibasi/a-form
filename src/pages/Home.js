82775e329de806943e927ccf41b25d3dee4113af
masibasi <jimin_lee98@naver.com>
2023-04-28 17:41:17 +0900

    feat : re-design home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,24 +3,34 @@ import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 import { FormList } from "../components/FormList";
+import Bg from "../assets/images/form-bg.png";
 
 export default function Home() {
     const navigation = useNavigate();
 
     return (
         <div className="Home">
-            <h1>A-Form</h1>
-            <p>An online form for everyone</p>
-            <div className="homeBtnWrapper">
-                <Button onClick={() => navigation("/create")}>
-                    Create Form
-                </Button>
-                <Button
-                    onClick={() => navigation("/about")}
-                    variant="outline-primary"
-                >
-                    About Form
-                </Button>
+            <div className="homeTopWrapper">
+                <div className="homeLeftWrapper">
+                    <h1 className="title">Welcome to our A-Form</h1>
+                    <p className="catchPhrase">An online form for everyone</p>
+                    <div className="homeBtnWrapper">
+                        <Button
+                            className="homeBtn"
+                            onClick={() => navigation("/create")}
+                        >
+                            Create Form
+                        </Button>
+                        <Button
+                            className="homeBtn"
+                            onClick={() => navigation("/about")}
+                            variant="outline-primary"
+                        >
+                            About Form
+                        </Button>
+                    </div>
+                </div>
+                <img src={Bg} className="homeImg" />
             </div>
             <FormList />
         </div>
