a73f12b0eb9e3c6801ee5a4a1a9920aaddbc1941
masibasi <jimin_lee98@naver.com>
2023-04-30 02:27:32 +0900

    working on about.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { FaGithub } from "react-icons/fa";
+import Logo2 from "../assets/images/logo2.png";
+import "./About.css";
 
 export default function About() {
     return (
-        <div>
-            About Developed by: <a href="https://acceler.kr">Team ACCELER</a>
+        <div className="About">
+            <div className="page1">
+                <img src={Logo2} />
+                <h4>
+                    <span className="A">A</span>mazing Form <br />
+                    <span className="A">A</span>utomatical Form <br /> By team{" "}
+                    <span className="A">A</span>CCELER !
+                </h4>
+            </div>{" "}
+            Developed by:
+            <a href="https://acceler.kr">Team ACCELER</a>
             <FaGithub size={24} href="https://github.com/KEA-ACCELER/a-form" />
         </div>
     );
