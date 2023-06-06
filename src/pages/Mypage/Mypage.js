e30d0495d05a8200589a308916a7d43314c2f3ed
masibasi <jimin_lee98@naver.com>
2023-06-06 11:34:30 +0900

    css : modify css of mypage

--- src/pages/Mypage/Mypage.js
+++ src/pages/Mypage/Mypage.js
@@ -16,120 +16,120 @@ import { GetUserPostsCnt } from "../../services/post/post.service";
 import { ButtonGroup, Button } from "react-bootstrap";
 
 export default function Mypage() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleSettingClick = () => {
-    navigate("/mypage_setting");
-  };
-  const { userToken, isLogin } = useContext(AuthenticationContext);
-
-  const CheckLogin = () => {
-    if (isLogin == false) {
-      alert("로그인이 필요한 서비스 입니다.");
-      navigate(-1);
-    }
-  };
-
-  useEffect(() => {
-    CheckLogin();
-  }, []);
-
-  // 유저 데이터 가져오기
-  const [userData, setUserData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await GetUserData(userToken);
-      setUserData(result);
+    const handleSettingClick = () => {
+        navigate("/mypage_setting");
     };
-    fetchData();
-  }, []);
+    const { userToken, isLogin } = useContext(AuthenticationContext);
 
-  //작성 설문수 가져오기
-  const [userPostsCount, setUserPostsCount] = useState(0);
-
-  useEffect(() => {
-    const fetchUserPostsCount = async () => {
-      if (userData) {
-        const result = await GetUserPostsCnt(userData.userPk);
-        setUserPostsCount(result);
-      }
+    const CheckLogin = () => {
+        if (isLogin == false) {
+            alert("로그인이 필요한 서비스 입니다.");
+            navigate(-1);
+        }
     };
 
-    fetchUserPostsCount();
-  }, [userData]);
+    useEffect(() => {
+        CheckLogin();
+    }, []);
 
-  //답변 설문수 받아오기
-  const [answeredSurveys, setAnsweredSurveys] = useState([]);
-  useEffect(() => {
-    const fetchAnsweredSurveys = async () => {
-      const result = await GetAnsweredSurveysTotal(1, 10, userToken); // 여기에서 page와 offset 값을 제공
-      setAnsweredSurveys(result);
-    };
+    // 유저 데이터 가져오기
+    const [userData, setUserData] = useState(null);
 
-    fetchAnsweredSurveys();
-  }, [userToken]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await GetUserData(userToken);
+            setUserData(result);
+        };
+        fetchData();
+    }, []);
 
-  return (
-    <FadeIn className="Mypage">
-      <div className="main">
-        <div className="profile">
-          <div className="profile_box">
-            <div className="profile_img">
-              <img src={profileimg} alt="" />
+    //작성 설문수 가져오기
+    const [userPostsCount, setUserPostsCount] = useState(0);
+
+    useEffect(() => {
+        const fetchUserPostsCount = async () => {
+            if (userData) {
+                const result = await GetUserPostsCnt(userData.userPk);
+                setUserPostsCount(result);
+            }
+        };
+
+        fetchUserPostsCount();
+    }, [userData]);
+
+    //답변 설문수 받아오기
+    const [answeredSurveys, setAnsweredSurveys] = useState([]);
+    useEffect(() => {
+        const fetchAnsweredSurveys = async () => {
+            const result = await GetAnsweredSurveysTotal(1, 10, userToken); // 여기에서 page와 offset 값을 제공
+            setAnsweredSurveys(result);
+        };
+
+        fetchAnsweredSurveys();
+    }, [userToken]);
+
+    return (
+        <FadeIn className="Mypage">
+            <div className="main">
+                <div className="profile">
+                    <div className="profile_box">
+                        <div className="profile_img">
+                            <img src={profileimg} alt="" />
+                        </div>
+
+                        <div className="nameline">
+                            <div className="profile_name">{userData?.name}</div>
+                            <div className="edit">
+                                <button className="edit_button" onClick={handleSettingClick}>
+                                    <img src={edit_icon} alt="" />
+                                </button>
+                            </div>
+                        </div>
+
+                        <div className="line"></div>
+
+                        <div className="profile_email">{userData?.email}</div>
+
+                        <div className="profile_post">
+                            <div className="post">작성 설문수</div>
+                            <div className="post_num" onClick={() => navigate("posted")} style={{ cursor: "pointer" }}>
+                                {userPostsCount}
+                            </div>
+                        </div>
+
+                        <div className="profile_response">
+                            <div className="response">답변 설문수</div>
+                            <div className="response_num" onClick={() => navigate("answered")} style={{ cursor: "pointer" }}>
+                                {answeredSurveys ? answeredSurveys.total : 0}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="Mypage_survey">
+                    <div className="btnGroupWrapper">
+                        <ButtonGroup>
+                            <Button variant="outline-primary" className="" onClick={() => navigate("template")}>
+                                나의 설문 템플릿
+                            </Button>
+                            <Button variant="outline-primary" className="" onClick={() => navigate("posted")}>
+                                내가 올린 설문
+                            </Button>
+                            <Button variant="outline-primary" className="" onClick={() => navigate("answered")}>
+                                내가 응답한 설문
+                            </Button>
+                        </ButtonGroup>
+                    </div>
+
+                    <Routes>
+                        <Route path="template" element={<MyTemplatePage />} />
+                        <Route path="posted" element={<MyPostedSurveysPage />} />
+                        <Route path="answered" element={<MyAnsweredSurveysPage />} />
+                    </Routes>
+                </div>
             </div>
-
-            <div className="nameline">
-              <div className="profile_name">{userData?.name}</div>
-              <div className="edit">
-                <button className="edit_button" onClick={handleSettingClick}>
-                  <img src={edit_icon} alt="" />
-                </button>
-              </div>
-            </div>
-
-            <div className="line"></div>
-
-            <div className="profile_email">{userData?.email}</div>
-
-            <div className="profile_post">
-              <div className="post">작성 설문수</div>
-              <div className="post_num" onClick={() => navigate("posted")} style={{ cursor: "pointer" }}>
-                {userPostsCount}
-              </div>
-            </div>
-
-            <div className="profile_response">
-              <div className="response">답변 설문수</div>
-              <div className="response_num" onClick={() => navigate("answered")} style={{ cursor: "pointer" }}>
-                {answeredSurveys ? answeredSurveys.total : 0}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="Mypage_survey">
-          <nav style={{ marginBottom: "20px" }}>
-            <ButtonGroup>
-              <Button variant="outline-primary" className="" onClick={() => navigate("template")}>
-                나의 설문 템플릿
-              </Button>
-              <Button variant="outline-primary" className="" onClick={() => navigate("posted")}>
-                내가 올린 설문
-              </Button>
-              <Button variant="outline-primary" className="" onClick={() => navigate("answered")}>
-                내가 응답한 설문
-              </Button>
-            </ButtonGroup>
-          </nav>
-
-          <Routes>
-            <Route path="template" element={<MyTemplatePage />} />
-            <Route path="posted" element={<MyPostedSurveysPage />} />
-            <Route path="answered" element={<MyAnsweredSurveysPage />} />
-          </Routes>
-        </div>
-      </div>
-    </FadeIn>
-  );
+        </FadeIn>
+    );
 }
--- src/pages/Mypage/Mypage_setting.js
+++ src/pages/Mypage/Mypage_setting.js
@@ -5,6 +5,8 @@ import alarm from "../../assets/images/alarm.png";
 import alarm_no from "../../assets/images/alarm_no.png";
 import { useNavigate } from "react-router-dom";
 import { AuthenticationContext } from "../../services/authentication/authentication.context";
+import { Button } from "react-bootstrap";
+import FadeIn from "../../animation/FadeIn";
 
 export default function Mypage_setting() {
     const navigate = useNavigate();
@@ -37,53 +39,54 @@ export default function Mypage_setting() {
 
     return (
         <div className="Mypage_setting">
-            <div className="setting_container1">
-                <div className="profile_img">
-                    <img src={previewImg ? previewImg : profileimg} alt="" className="profile_picture" />
+            <FadeIn className="settingWrapper">
+                <div className="setting_container1">
+                    <div className="profile_img">
+                        <img src={previewImg ? previewImg : profileimg} alt="" className="profile_picture" />
+                    </div>
+
+                    <button className="button change_photo_button" onClick={() => document.getElementById("profileImg").click()}>
+                        Change Photo
+                    </button>
+                    <input onChange={handleImageChange} className="change_photo_button_input" type="file" accept="image/*" id="profileImg" />
                 </div>
 
-                <button className="button change_photo_button" onClick={() => document.getElementById("profileImg").click()}>
-                    Change Photo
-                </button>
-                <input onChange={handleImageChange} className="change_photo_button_input" type="file" accept="image/*" id="profileImg" />
-            </div>
+                <div className="setting_container2">
+                    <div className="your_profile">
+                        <div className="your_profile_item">
+                            <div className="your_profile_title">Your name</div>
+                            <input type="text" className="your_profile_name" value={name} onChange={(e) => setName(e.target.value)} />
+                        </div>
+                        <div className="your_profile_item">
+                            <div className="your_profile_title">Your Phone Number</div>
+                            <input type="text" className="your_profile_nickname" value={phone} onChange={(e) => setPhone(e.target.value)} />
+                        </div>
+                        <div className="your_profile_item">
+                            <div className="your_profile_title">Your email</div>
+                            <input type="email" className="your_profile_email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div className="your_profile_item">
+                            <div className="your_profile_title">Change password</div>
+                            <input type="password" className="your_profile_password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                    </div>
 
-            <div className="setting_container2">
-                <div className="setting_buttons">
-                    <button className="back_button">Back</button>
-                    <button className="save_changes_button">Save Changes</button>
-                </div>
+                    <div className="alarm_setting">
+                        <div className="alarm_icon" onClick={toggleAlarm}>
+                            <img src={isAlarmEnabled ? alarm : alarm_no} alt="" />
+                        </div>
+                        <div className="alarm_option">Enable Notification</div>
+                    </div>
 
-                <div className="your_profile">
-                    <div className="your_profile_item">
-                        <div className="your_profile_title">Your name</div>
-                        <input type="text" className="your_profile_name" value={name} onChange={(e) => setName(e.target.value)} />
-                    </div>
-                    <div className="your_profile_item">
-                        <div className="your_profile_title">Your Phone Number</div>
-                        <input type="text" className="your_profile_nickname" value={phone} onChange={(e) => setPhone(e.target.value)} />
-                    </div>
-                    <div className="your_profile_item">
-                        <div className="your_profile_title">Your email</div>
-                        <input type="email" className="your_profile_email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div className="your_profile_item">
-                        <div className="your_profile_title">Change password</div>
-                        <input type="password" className="your_profile_password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <div className="setting_buttons">
+                        <Button variant="outline-danger" onClick={() => navigate(-1)}>
+                            Back
+                        </Button>
+                        <Button variant="outline-primary">Save Changes</Button>
+                        <Button variant="warning">LOGOUT</Button>{" "}
                     </div>
                 </div>
-
-                <div className="alarm_setting">
-                    <div className="alarm_icon" onClick={toggleAlarm}>
-                        <img src={isAlarmEnabled ? alarm : alarm_no} alt="" />
-                    </div>
-                    <div className="alarm_option">Enable Notification</div>
-                </div>
-
-                <div className="logout_button_container">
-                    <button className="logout_button">LOGOUT</button>{" "}
-                </div>
-            </div>
+            </FadeIn>
         </div>
     );
 }
